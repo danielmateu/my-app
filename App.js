43cca8261dde531439f0a2f201f27dc0ddd341e9
Daniel Mateu <danielmateu86@gmail.com>
2022-09-08 09:49:00 +0200

    First contact with React Native build with EXPO... Amazing!

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my first contact with Reat Native and EXPO 🙃</Text>
+      <Text>And looks awesome! 💀</Text>
       <StatusBar style="auto" />
     </View>
   );
